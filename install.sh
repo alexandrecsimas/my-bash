7f4f71ddbe5a8a36a3810ce4e4ac339df8b57af0
Alexandre Carvalho <50042620+xandycarvalho@users.noreply.github.com>
2023-08-21 23:20:53 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+
+sudo apt update
+
+# scrcpy - espelhar celular no pc
+sudo apt install scrcpy -y
+
+sudo apt install git -y
+git config --global user.name "Alexandre Carvalho"
+git config --global user.email alexandrecsimas@gmail.com
+git config --global core.editor nano
+
+mkdir /home/$USER/github/
+
+# python
+# sudo apt install xclip python3 python3-pip python3-devel
+# sudo pip3 install ash-editor
+
+
+
